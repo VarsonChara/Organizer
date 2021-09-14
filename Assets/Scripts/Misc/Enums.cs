0bbe81de29d89c09a035bbfa334f89865dd00e59
Tails exe <mareksmrekrg@gmail.com>
2021-09-14 20:18:24 +0200

    Color palettes & some container code
    Added basic color palete to the project.
    Created first container for data with test example.

---
+++ Assets/Scripts/Misc/Enums.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum FlairType
+{
+    Misc,
+    Game,
+    VisualNovel,
+    Book,
+    Anime,
+    Learning,
+    Work,
+    ToDo
+}
+
+
+public class Enums
+{
+    //TODO: If some operations on enums are needed, here singleton can be created to simplify them and have in one place
+}

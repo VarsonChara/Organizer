db15c22352a38b16cfc1f8a5ef7850ada5444cda
Tails exe <mareksmrekrg@gmail.com>
2021-09-23 16:32:00 +0200

    Button names, sprite and fixes to resolutions
    Added button names.
    Added self made pixelart button.
    Fixed the UI breaking when the resolution was changed due to rescaling the window.

---
+++ Assets/Scripts/ContentControllers/ScrollRectTop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectTop : MonoBehaviour
+{
+    void Start()
+    {
+        //Needs to wait a frame
+        StartCoroutine(WaitAFrame());
+    }
+
+    IEnumerator WaitAFrame()
+    {
+        yield return null;
+
+        //Get the rectTransform
+        ScrollRect sr = GetComponent<ScrollRect>();
+        RectTransform rt = sr.content;
+
+        //Set the Y to the top
+        Vector3 startPos = rt.anchoredPosition;
+ 
+        //Divide by -2, but faster
+        startPos.y = rt.sizeDelta.y * -.5f;
+        rt.anchoredPosition = startPos;
+    }
+}

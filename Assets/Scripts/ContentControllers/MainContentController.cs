27a95048a25841496b3fed425ddd1d7534918759
Tails exe <mareksmrekrg@gmail.com>
2021-09-26 18:56:19 +0200

    Content recognition, prepared the script for savefile

--- Assets/Scripts/ContentControllers/MainContentController.cs
+++ Assets/Scripts/ContentControllers/MainContentController.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainContentController : MonoBehaviour
 {
+
+    [SerializeField]
+    private ContentScriptable[] contentTypes;
+
     /// <summary>
     /// The target where content will be stored, first instance of this has VerticalGroup in it.
     /// </summary>
@@ -16,11 +22,18 @@ public class MainContentController : MonoBehaviour
     /// </summary>
     private List<Content> contents = new List<Content>();
 
+    
+    [Header("Save File parameters")]
     [SerializeField]
-    private ScrollRect scrollRect;
+    private string saveFileName = "SaveFile";
 
+    private string saveFileCompletePath;
+
+
+    [Space]
+
+    [Header("Testing")]
     // TODO: REMOVE OR REPURPOSE
-    public ContentScriptable testScriptable;
     public GameObject testContent;
 
     private void Awake()
@@ -31,6 +44,7 @@ public class MainContentController : MonoBehaviour
             return;
         }
 
+
         //Temporary gameobject and content for instantiating
         GameObject tmpGo;
         Content tmpContent;
@@ -38,16 +52,61 @@ public class MainContentController : MonoBehaviour
         //TODO: Repurpose it to be loading from file, also create a function that create new content while playing the app
         //TODO: Change it to coroutine, so the app can load and stay responsive, regardless of the amount of content loaded
         //TODO: A loading bar or circle implementation would be nice
+
+        saveFileCompletePath = saveFileName + ".json";
+
+        if (!File.Exists(saveFileCompletePath))
+        {
+            File.Create(saveFileCompletePath);
+        }
+
+        
+
+
+        // TODO: Remove completly
         for (int i = 0; i < 10; i++)
         {
+
             tmpGo = Instantiate(testContent, contentContainer);
             tmpContent = tmpGo.GetComponent<Content>();
             contents.Add(tmpContent);
 
-            tmpContent.contentScriptable = testScriptable;
+            tmpContent.contentScriptable = DetermineContent((FlairType)UnityEngine.Random.Range(0,contentTypes.Length - 1));
             tmpContent.SetContent(i.ToString());
 
         }
         
     }
+
+
+    public ContentScriptable DetermineContent(FlairType flair)
+    {
+
+        foreach (var cType in contentTypes)
+        {
+            if (cType.flairType == flair) return cType;
+        }
+
+        Debug.LogError("Content not found! Critical error!");
+
+        Debug.LogError((int)flair);
+
+#if UNITY_EDITOR
+        Debug.Break();
+#else
+        // TODO: Needs to implement feedback on app crash
+        Application.Quit();
+#endif
+        return null;
+    }
+
+
+    private void OnApplicationQuit()
+    {
+        
+
+        //Debug.Log(save);
+        //File.WriteAllText(saveFileCompletePath, save);
+    }
+
 }
